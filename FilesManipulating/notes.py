8030515d74c51a19e50529e561478d519a1e9a1f
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-02-21 13:24:42 +0000

    Class

--- FilesManipulating/notes.py
+++ FilesManipulating/notes.py
@@ -1,15 +1,26 @@
-from io import *
+# from io import *
 
-archivo = open(r"FilesManipulating/archivo.txt", "w")
+# archivo = open(r"FilesManipulating/archivo.txt", "w")
 
-archivo.write("Hola mundo\n")
+# archivo.write("Hola mundo\n")
 
-archivo.close()
+# archivo.close()
 
-print(f"archivo.txt creado en {archivo.name}")
+# print(f"archivo.txt creado en {archivo.name}")
 
-archivo = open(r"FilesManipulating/archivo.txt", "r")
-print (f"{archivo.read()}")
+# archivo = open(r"FilesManipulating/archivo.txt", "r")
+# print (f"{archivo.read()}")
+
+class Example:
+    #atributos
+    subject = "Python"
+    num_hours = 3
+    
+    def count_hours(self):
+        pass
+    def number_hours(self):
+        pass
+    
 
 
 
