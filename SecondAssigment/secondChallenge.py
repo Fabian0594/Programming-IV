33d33c21e0b12686c5edbfd7aef36017e93e4e20
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-02-24 02:42:34 +0000

    In progress

--- SecondAssigment/secondChallenge.py
+++ SecondAssigment/secondChallenge.py
@@ -1,47 +1,81 @@
-from io import *
-
-#Realice un programa que recorra una lista llena de los objetos usuario que quieran, luego van a contar la cantidad que un usuario aparezca en la lista y luego debe decir si aparece una cantidad par o impar de veces. al diseñar las clases usted debe considerar los atrobutos que crea necesarios.
-
-class user:
-    
-    profession = ""
-    name = ""
-    age = 0
-    
-    def insertData(self):
+class User:
+    def insert_data(self):
         self.profession = input("Enter your profession: ")
         self.name = input("Enter your name: ")
-        self.age = int(input("Enter yor age: "))
-        
-    def returnData(self):
+        self.age = int(input("Enter your age: "))
+    
+    def return_data(self):
         return f"User(\nName: {self.name}\nProfession: {self.profession}\nAge: {self.age}\n )"
     
-    def compareByName(self, other):
+    def compare_by_name(self, other):
         return self.name == other.name
-        
-    def compareByProfession(self, other):
+    
+    def compare_by_profession(self, other):
         return self.profession == other.profession
     
-    def compareByAge(self, other):
+    def compare_by_age(self, other):
         return self.age == other.age
-        
-        
-        
-def fillTheList():
-        usersList = []
-        while True:
-            userTemp = user()
-            userTemp.insertData()
-            usersList.append(userTemp)
-            if int(input("Enter 0 to exit or another number to continuo adding users: \n")) == 0:
-                break;
-        return usersList
     
 
-          
-          
-users = fillTheList()
+def fill_the_list():
+    users_list = []
+    while True:
+        user_temp = User()
+        user_temp.insert_data()
+        users_list.append(user_temp)
+        if int(input("Enter 0 to exit or another number to continue adding users: \n")) == 0:
+            return users_list
 
-for i in range(len(users) - 1):
-    print(users[i].compareByAge(users[i + 1]))   
+
+def count_occurrences_by_name(users, element):
+    return sum(1 for user in users if user.name == element)
+
+
+def count_occurrences_by_age(users, element):
+    return sum(1 for user in users if user.age == element)
+
+
+def count_occurrences_by_profession(users, element):
+    return sum(1 for user in users if user.profession == element)
+
+
+def is_even(number):
+    return number % 2 == 0
+
+def menu():
+    global user_list  # Usar la lista global
+    print("Welcome\nSelect your option:\n1. Create a list and enter data\n2. Count occurrences by Name\n3. Count occurrences by Age\n4. Count occurrences by Profession")
+    option = int(input())
     
+    if option == 1:
+        user_list = fill_the_list()
+    elif option == 2:
+        name_to_find = input("Enter the name: ")
+        occurrences_by_name = count_occurrences_by_name(user_list, name_to_find)
+        print(occurrences_by_name)
+        print(f"The times {name_to_find} appears is {'even' if is_even(occurrences_by_name) else 'odd'}\n")
+    elif option == 3:
+        age_to_find = int(input("Enter the age: "))
+        occurrences_by_age = count_occurrences_by_age(user_list, age_to_find)
+        print(occurrences_by_age)
+        print(f"The times {age_to_find} appears is {'even' if is_even(occurrences_by_age) else 'odd'}\n")
+    elif option == 4:
+        profession_to_find = input("Enter the profession: ")
+        occurrences_by_profession = count_occurrences_by_profession(user_list, profession_to_find)
+        print(occurrences_by_profession)
+        print(f"The times {profession_to_find} appears is {'even' if is_even(occurrences_by_profession) else 'odd'}\n")
+    else:
+        print("Invalid option. Exiting...")
+        return
+    
+    option = input("Do you want to go back? (y/n): ")
+    if option.lower() == 'y':
+        menu()
+    else:
+        print("Goodbye!")
+        
+def main():
+    user_list = []
+    menu()
+
+main()
