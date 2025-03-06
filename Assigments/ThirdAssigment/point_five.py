95d5ee59397959d671206b283802052020421f75
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-03-06 03:38:17 +0000

    m

--- Assigments/ThirdAssigment/point_five.py
+++ Assigments/ThirdAssigment/point_five.py
@@ -1,5 +1,10 @@
 class list_operations:
     
     def __init__(self):
+        self.original_list = [1, 1991, "taller", 200, 3, "programación", 700, "utp", "POO"]
         self.int_list = []
         self.str_list = []
+        self.replaced_list = []
+        
+    def insert_data_to_file(self, data):
+        with(open(""))
