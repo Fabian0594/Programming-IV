cf0652d5d6b6274bb9a70049111ce12db7863c14
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-03-06 20:41:53 +0000

    Five Done

--- Assigments/ThirdAssigment/point_five.py
+++ Assigments/ThirdAssigment/point_five.py
@@ -1,10 +1,30 @@
-class list_operations:
-    
-    def __init__(self):
-        self.original_list = [1, 1991, "taller", 200, 3, "programación", 700, "utp", "POO"]
-        self.int_list = []
-        self.str_list = []
-        self.replaced_list = []
-        
-    def insert_data_to_file(self, data):
-        with(open(""))
+import json
+
+def save_list(lst, filename):
+    path = r"/workspaces/Programming-IV/Assigments/ThirdAssigment/" + filename
+    with open(path, "w") as f:
+        json.dump(lst, f)
+
+def separate_list(lst):
+    numbers = [x for x in lst if isinstance(x, int)]
+    strings = [x for x in lst if isinstance(x, str)]
+    save_list(numbers, "numbers.json")
+    save_list(strings, "strings.json")
+
+def replace_odds(lst):
+    new_list = ["reemplazo" if isinstance(x, int) and x % 2 != 0 else x for x in lst]
+    save_list(new_list, "replacement.json")
+
+def print_half(lst):
+    half = len(lst) // 2
+    print(lst[:half])
+
+lst = [1, 1991, "taller", 200, 3, "programacion", 700, "utp", "POO"]
+
+save_list(lst, "original_list.json")
+
+separate_list(lst)
+
+replace_odds(lst)
+
+print_half(lst)
